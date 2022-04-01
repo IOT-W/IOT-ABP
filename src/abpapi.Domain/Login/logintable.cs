df01230eff946152cf2c7a1703df5486a97ddb39
WuJunHaoQ <wujunhao511@outlook.com>
2022-04-01 10:03:28 +0800

    Wu

--- src/abpapi.Domain/Login/logintable.cs
+++ src/abpapi.Domain/Login/logintable.cs
@@ -11,22 +11,29 @@ namespace abpapi.Login
     public class logintable : Entity<Guid>
     {
         /// <summary>
+        /// 支付宝用户id
+        /// </summary>
+        public string Login_userid { get; set; }
+        /// <summary>
         /// 用户账号
         /// </summary>
         public string Login_account { get; set; }
-
         /// <summary>
         /// 用户密码
         /// </summary>
         public string Login_pwd { get; set; }
 
         /// <summary>
-        /// 用户名称
+        /// 支付宝用户名称
         /// </summary>
         public string Login_name { get; set; }
+        /// <summary>
+        /// 用户头像
+        /// </summary>
+        public string Login_avatar { get; set; }
 
         /// <summary>
-        /// 账号类型
+        /// 账号状态
         /// </summary>
         public int Login_state { get; set; }
     }
